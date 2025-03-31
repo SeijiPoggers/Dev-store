f6584941a7739087d331b70e8a6bc3c117f1c3d0
SeijiPoggers <otavio.seiji.1bmangal@gmail.com>
2025-03-31 15:52:27 -0300

    Add files via upload

--- produtos.php
+++ produtos.php
@@ -1,12 +1,20 @@
 <?php
+    //cabeçalhos para o navegador
     header("Content-Type:application/json");
     header("charset=utf-8");
+    //chama a conexao
     include 'db.php';
+    //cria uma variavel para receber o método
+    //da requisicao
     $method = $_SERVER['REQUEST_METHOD'];
-    $input = json_decode(file_get_contents('php://input'), true, JSON_UNESCAPED_UNICODE);
-
-    switch ($method) {
-        case 'GET':
+    //cria uma variavel para armazenar 
+    // os dados enviados do frontend
+    $input = json_decode(
+        file_get_contents('php://input'), true,
+        JSON_UNESCAPED_UNICODE);
+    //VERIFICA O METODO REQUISITADO
+    switch($method){
+        case 'GET'://caso seja get, faz a consulta
             if(isset($_GET['descProd'])){
                 handleGetFiltro($pdo);
             }else if(isset($_GET['idProd'])){
@@ -17,13 +25,19 @@
                 handleGet($pdo);
             }
     }
-
+    //função para fazer a consulta sem filtro
     function handleGet($pdo){
+        //cria o sql para ser executado
         $sql = "SELECT * from tblProdutos";
+        //prepara a execução
         $stmt = $pdo->prepare($sql);
+        //executa a consulta na base
         $stmt->execute();
-        $result = $stmt ->fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode($result, JSON_UNESCAPED_UNICODE);
+        //variavel para armazenar os resultados
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //exibe em formato json
+        echo json_encode($result, 
+        JSON_UNESCAPED_UNICODE);
     }
-    
+
 ?>
